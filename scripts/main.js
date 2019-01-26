63a875e70aae4f54a84e400539591bafdbf2ea00
kleber24021 <kleber24021@gmail.com>
2019-01-26 22:57:00 +0100

    Side Navbar fully functional
    IDK what have I done, but it works.

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
 function openNav() {
   document.getElementById("sidenavbar").style.width = "480px";
-  document.getElementById("content").style.left = "480px";
+  // document.getElementById("content").style.left = "480px";
   // document.getElementById("closebutton").style.width = "50px";
 }
 function closeNav(){
   document.getElementById("sidenavbar").style.width = "0px"
-  document.getElementById("content").style.left = "0px";
+  // document.getElementById("content").style.left = "0px";
     // document.getElementById("closebutton").style.width = "0px";
 }
