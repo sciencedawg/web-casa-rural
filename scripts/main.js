cee84de4b4b36f68428f278baeb2a214345b1943
kleber24021 <kleber24021@gmail.com>
2019-01-27 11:27:26 +0100

    Added navbar/content animations and color pallete to CSS.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 function openNav() {
   document.getElementById("sidenavbar").style.width = "480px";
+  document.getElementById('content').style.padding = "60px 0 0 480px";
 }
 function closeNav(){
   document.getElementById("sidenavbar").style.width = "0px"
+  document.getElementById('content').style.padding = "60px 0 0 0";
 }
