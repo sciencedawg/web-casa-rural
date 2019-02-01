d72f160c754a288f779bc0bc8eefa899e5e47503
kleber24021 <kleber24021@gmail.com>
2019-02-01 17:43:29 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,9 @@
 function toggleSideNav() {
   const sideNavBar = document.getElementById('sidenavbar');
-  if (sideNavBar.style.left === '0px'){
+  if (sideNavBar.style.left === '0px') {
     sideNavBar.style.left = '-420px';
     document.getElementById('menuicon').classList = 'fas fa-bars';
-
-  }
-  else {
+  } else {
     sideNavBar.style.left = '0px';
     document.getElementById('menuicon').classList += 'fas fa-caret-left';
   }
