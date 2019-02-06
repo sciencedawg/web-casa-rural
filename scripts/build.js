a64302e4e046ea94bc2923db00c5333efb3c18cd
bmcaos <bmartinenac@gmail.com>
2019-02-06 19:04:34 +0000

    Tab changing start. Needs fixing. I'm lost.

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,23 @@
+function change_tab(jsonfile, tabname) {
+  console.log(tabname);
+  let tabname_a = tabname.replace('<span class="topnavmenuitem">','').replace('</span>', '');
+  console.log(tabname_a);
+  $.getJSON(jsonfile, function(data) {
+    doc = data['html'];
+    console.log(doc);
+    $('#sidenavmenu_u')[0].innerHTML = "";
+    console.log(doc['sidenavmenu'][tabname_a]);
+    for (var i = 0; i < doc['sidenavmenu'][tabname_a].length; i++) {
+      let contentdiv = doc['sidenavmenu'][tabname_a];
+      let item = "<a href='#" + contentdiv[i] + "'><li class='sidenavmenuitem'>" + contentdiv[i] + "</li></a>";
+      console.log(item);
+      $('#sidenavmenu_u')[0].innerHTML += item;
+    }
+  })
+}
+
 function build_page(jsonfile) {
+  let lang_json = jsonfile;
   $.getJSON(jsonfile, function(data) {
     doc = data['html'];
     $('title')[0].innerHTML = doc['title'];
