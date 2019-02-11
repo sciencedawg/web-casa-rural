975a6846b1b8c8e9feab403a00faa0d3e51c2696
bmcaos <bmartinenac@gmail.com>
2019-02-11 19:50:24 +0000

    Something's happening for sure.

--- scripts/build.js
+++ scripts/build.js
@@ -111,28 +111,24 @@ let page_lang;
 function includeHTML() {
  var z, i, elmnt, file, xhttp;
  /* Loop through a collection of all HTML elements: */
- z = document.getElementsByTagName("*");
- for (i = 0; i < z.length; i++) {
-   elmnt = z[i];
-   /*search for elements with a certain atrribute:*/
-   file = elmnt.getAttribute("w3-include-html");
-   if (file) {
-     /* Make an HTTP request using the attribute value as the file name: */
-     xhttp = new XMLHttpRequest();
-     xhttp.onreadystatechange = function() {
-       if (this.readyState == 4) {
-         if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-         if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-         /* Remove the attribute, and call this function once more: */
-         elmnt.removeAttribute("w3-include-html");
-         includeHTML();
-       }
+ z = $('#content')[0];
+ file = z.getAttribute("w3-include-html");
+ if (file) {
+   /* Make an HTTP request using the attribute value as the file name: */
+   xhttp = new XMLHttpRequest();
+   xhttp.onreadystatechange = function() {
+     if (this.readyState == 4) {
+       if (this.status == 200) {z.innerHTML = this.responseText;}
+       if (this.status == 404) {z.innerHTML = "Page not found.";}
+       /* Remove the attribute, and call this function once more: */
+       z.removeAttribute("w3-include-html");
+       includeHTML();
      }
-     xhttp.open("GET", file, true);
-     xhttp.send();
-     /* Exit the function: */
-     return;
    }
+   xhttp.open("GET", file, true);
+   xhttp.send();
+   /* Exit the function: */
+   return;
  }
 }
 
@@ -141,6 +137,7 @@ function change_tab(tabname) {
   let content = $('#content')[0]
   let doc = page_lang['html'];
   content.setAttribute('w3-include-html', doc['content'][tabname_a]['file']);
+  includeHTML();
   $('#sidenavmenu_u')[0].innerHTML = "";
   for (var i = 0; i < doc['sidenavmenu'][tabname_a].length; i++) {
     let contentdiv = doc['sidenavmenu'][tabname_a];
@@ -173,9 +170,10 @@ function scrolltopage() {
     }
     else {
       $('#head')[0].focus();
-    }
-
-  });
-}
+    }});}
+function scrolltopage_alt(){
+  if (window.pageYOffset == '0'){
+    scrolltopage();
+  }}
 
 build_page(es)
