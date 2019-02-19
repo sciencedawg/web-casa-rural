20fb79758a13a45fd57adee80f77833b95a0dd9f
sciencedawg <marco.martin.ludeiro@gmail.com>
2019-02-19 16:19:39 +0100

    Merge branch 'master' of https://github.com/sciencedawg/web-casa-rural

---
+++ scripts/temp.js
@@ -0,0 +1,393 @@
+const es = {
+  'lang':'es',
+  'page':{
+    'title':'Las Jordanas - Casa Rural',
+    'maintitle_a':'Casa Rural',
+    'godownbt':'Explorar',
+    'godownbt_wait':'Cargando',
+    'backtopbt':'Volver al título',
+    'logo':'';
+    'structure':[
+      {
+        'tab_name':'Inicio',
+        'tab_content':[
+          {
+            'section_name':'Bienvenidos',
+            'section_paragraphs':[
+              'La casa rural Las Jordanas se encuentra en el Parque Natural de Calblanque. A 2 km de las playas (unos 20 minutos a pie o 5 minutos en coche), y a 25 km de Cartagena; nuestra casa se halla en plena naturaleza, aislada, y a 2 km de Los Belones, el pueblo más cercano. Puede llegarse fácilmente desde las autovías A-30, AP-7 y RM-12, aunque si usan navegador les recomendamos o bien llamarnos al llegar a Los Belones, o bien consultar las instrucciones ofrecidas a continuación, ya que los navegadores indican erroneamente una vía en mal estado.'
+            ],
+          },
+          {
+            'section_name':'Nuestra historia',
+            'section_paragraphs':[
+              'Todo comenzó en 2010, después de años enamorados de esas playas salvajes, solitarias y de aguas limpias. Se cruzó en nuestra vida la casa de Las Jordanas, curiosa historia cuyos protagonistas fueron antiguos pobladores pertenecientes a la misma familia que habitó la casa ininterrumpidamente desde principios del siglo XIX: la Mañe, Los Hernández, el inmenso olivo del huerto, los palmitos, los granados... y que te contaremos con entusiasmo y cariño. La casa resolvía siempre nuestro problema cuando queríamos ir a Calblanque: ¿dónde vamos a dormir?¿En La Manga?¿En Cartagena?¿Cabo de Palos?¿Los Belones, quizás?... Se acabó: dormiríamos al fin escuchando al búho real por la noche, a los pájaros al despertar, viendo a las águilas sobrevolando el monte, oliendo el aroma del romero y del tomillo acariciados por la brisa del mar... junto a una de las mejores playas del Mediterráneo que aún quedan vírgenes.',
+              'Tras un periodo de reformas que se nos hizo eterno, lleno de obstáculos y cómo no, una ingente cantidad de procesos burocráticos, ¡lo logramos! Podemos ahora disfrutar del Parque Natural de Calblanque en todas sus facetas y en todas las épocas del año. Pero también se ve cumplido este sueño para ti, querido viajero y amante de la naturaleza, de la tranquilidad, de lo distinto... pues este entorno privilegiado es ahora accesible para ti desde la finalización de este proyecto el 1 de junio de 2013, disfrutando esta maravillosa experiencia con nosotros.',
+              'Estarás en un lugar perfecto donde descansar y disfrutar del Mare Nostrum. Encontrarás una casa del siglo XIX rehabilitada de la que hemos intentado respetar su estilo de casa de huerta murciana, levantina, mediterránea... Haciendo uso de materiales tradicionales y respetando al máximo todo lo que había en la casa antes de su rehabilitación, dispondrás en plena naturaleza de todas las comodidades, pudiendo elegir entre conectarte a Internet o desconectar y dejarte envolver por la naturaleza. Podrás pasear llegando a la cima del Cabezo de La Fuente y extasiarte con sus espectaculares vistas, recorrer la senda Circular al Cabezo de La Fuente, caminar a las playas de Calblanque, Salinas del Rasall, Negrete, Parreño, Dentoles, Las Mulas... Excursiones para todos los gustos. Podrás hacer uso además de nuestras bicicletas o montar a caballo si así lo prefieres. Si buscas unas vacaciones relajantes y no hacer mucho, nuestra casa será también tu rincón favorito. Podrás pasar agradables momentos en la antigua huerta, ya sea leyendo un libro o a la sombra del olivo de más de 300 años. Dejándote abrazar por los enormes palmitos, tomando el sol en las terrazas, echándote una siesta... ¡Te sentirás como en casa!'
+            ],
+          },
+          {
+            'section_name':'¿Por qué elegirnos?',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+        ]
+      },
+      {
+        'tab_name':'Galería',
+        'tab_content':[
+          {
+            'section_name':'Casa',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Habitaciones',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Espacios Comunes',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Terraza',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Paisajes',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+        ]
+      },
+      {
+        'tab_name':'Información',
+        'tab_content':[
+          {
+            'section_name':'Cómo Llegar',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Tarifas',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Tiempo',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Actividades',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Otros',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+        ]
+      },
+      {
+        'tab_name':'Contacto',
+        'tab_content':[
+          {
+            'section_name':'Información de Contacto',
+            'section_paragraphs':[
+              'Puede contactarnos en: casalasjordanas@gmail.com o bien en +34 610 605 287 por Whatsapp o voz de 15h a 23h.'
+            ],
+          },
+          {
+            'section_name':'Síguenos en Twitter',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Créditos Página Web',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+        ]
+      }
+    ]
+  }
+}
+
+
+const en = {
+  'lang':'en',
+  'page':{
+    'title':'Las Jordanas - Holiday Cottage',
+    'maintitle_a':'Holiday Cottage',
+    'godownbt':'Explore',
+    'godownbt_wait':'Loading',
+    'backtopbt':'Go back to the title',
+    'logo':'';
+    'structure':[
+      {
+        'tab_name':'Home',
+        'tab_content':[
+          {
+            'section_name':'Welcome',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Our story',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Why pick us?',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+        ]
+      },
+      {
+        'tab_name':'Galery',
+        'tab_content':[
+          {
+            'section_name':'House',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Bedrooms',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Shared spaces',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Garden and outside',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Views and landscapes',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+        ]
+      },
+      {
+        'tab_name':'Information',
+        'tab_content':[
+          {
+            'section_name':'Accesing the house',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Prices',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Weather',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Activities',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Other',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+        ]
+      },
+      {
+        'tab_name':'Contact Us',
+        'tab_content':[
+          {
+            'section_name':'Contact Info',
+            'section_paragraphs':[
+              'Our email is: casalasjordanas@gmail.com and our phone number is +34 610 605 287. Call us from 15h to 23h or text us through Whatsapp.'
+            ],
+          },
+          {
+            'section_name':'Follow us on Twitter',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Webmasters',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+        ]
+      }
+    ]
+  }
+}
+
+
+const fr = {
+  'lang':'fr',
+  'page':{
+    'title':'Las Jordanas - Maison Rurale',
+    'maintitle_a':'Maison Rurale',
+    'godownbt':'Explorer',
+    'godownbt_wait':'Chargement en cours',
+    'backtopbt':'Retour au titre',
+    'logo':'';
+    'structure':[
+      {
+        'tab_name':'Accueil',
+        'tab_content':[
+          {
+            'section_name':'Bienvenue',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Notre histoire',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Pourquoi nous choisir?',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+        ]
+      },
+      {
+        'tab_name':'Images',
+        'tab_content':[
+          {
+            'section_name':'Notre maison',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Nos chambres',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Nos espaces communs',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Notre terrasse',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Paysages',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+        ]
+      },
+      {
+        'tab_name':'Infos pratiques',
+        'tab_content':[
+          {
+            'section_name':'Comment y aller',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Tarif',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Météo',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Activités',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Autre',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+        ]
+      },
+      {
+        'tab_name':'Nous contacter',
+        'tab_content':[
+          {
+            'section_name':'Information de contact',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Twitter',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+          {
+            'section_name':'Crédits de la page web',
+            'section_paragraphs':[
+              ''
+            ],
+          },
+        ]
+      }
+    ]
+  }
+}
